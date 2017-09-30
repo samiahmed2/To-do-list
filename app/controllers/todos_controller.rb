75fafef2dd2bc91c54b992a3ba3c550d699e6203
Sami Ahmed <sami.ahmed@aecicharterhs.org>
2017-09-30 19:19:38 +0000

    style css

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,19 +1,28 @@
 class TodosController < ApplicationController
 
 def index 
+    @code_word= "#{params[:code]} #{params[:word]}"
 end
 
 def show 
-    @todo={
-        id: params[:id],
-        name:'wake up at 8am',
+    id= params[:id]
+    if id == '1'
     
-        description: 'Mon to Fri',
-        duration: 15
+        @todo={
+            id: id,
+            name: 'wake up at 8am',
+            duration: 15
+        }
+    elsif id == "2"
+        @todo= {
+            id: params[:id],
+        name: 'I am tired',
+        description: 'All day',
+        duration:25
     }
+    else
+        @todo= {}
+    end
+    
 end
-
-
-
-
 end
