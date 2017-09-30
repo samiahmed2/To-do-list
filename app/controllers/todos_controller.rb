babaacd2c3bac84ac5a13c83685e91a7b60176c8
Sami Ahmed <sami.ahmed@aecicharterhs.org>
2017-09-30 17:53:41 +0000

    desciption-id-updated show.html

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,15 @@ class TodosController < ApplicationController
 def index 
 end
 
-
+def show 
+    @todo={
+        id: params[:id],
+        name:'wake up at 8am',
+    
+        description: 'Mon to Fri',
+        duration: 15
+    }
+end
 
 
 
