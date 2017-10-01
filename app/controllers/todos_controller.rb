1c7a12ec19ef3b7f193599116a51a5c40a0c2b32
Sami Ahmed <sami.ahmed@aecicharterhs.org>
2017-10-01 18:42:49 +0000

    showpage

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,25 +4,31 @@ def index
     @code_word= "#{params[:code]} #{params[:word]}"
 end
 
-def show 
-    id= params[:id]
-    if id == '1'
-    
-        @todo={
-            id: id,
-            name: 'wake up at 8am',
-            duration: 15
-        }
-    elsif id == "2"
-        @todo= {
-            id: params[:id],
-        name: 'I am tired',
-        description: 'All day',
-        duration:25
-    }
-    else
-        @todo= {}
-    end
-    
+def create
+    todo= Todo.create(
+    name:params[:name],
+    description:params[:description],
+    duration:params[:duration],
+    compleate:params[:compleate]
+
+        )
+    redirect_to "/todos/#{todo.id}"    
+
 end
+
+def show 
+    @todo=Todo.find(params[:id])
+end
+
+def update
+ todo=Todo.find(params[:id])
+ todo.update(
+    name:params[:name],
+    description:params[:description],
+    duration:params[:duration],
+    compleate:params[:compleate]
+ )
+ redirect_to "/todos/#{todo.id}"
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   root 'todos#index'
   get 'todos' => 'todos#index'
   get 'todos/:id' => 'todos#show'
-  
- 
-  
-
- 
+  post 'todos' => 'todos#create'
+  post 'todos/:id'=>'todos#update'
 end
